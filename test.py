ad6303c7e2569cf2a8d4fe5c318ff57b894b57df
jaehong <tmdwoghd@naver.com>
2023-07-28 17:25:06 +0900

    0728

--- test.py
+++ test.py
@@ -1,27 +1,8 @@
-# ws_7_4.py
+# ws_8_2.py
 
 # 아래 클래스를 수정하시오.
-class Shape:
-    def __init__(self,width,height):
-        self.width = width
-        self.height = height
-    
-    def calculate_area(self):
-        return self.width * self.height
-    
-    def calculate_perimeter(self):
-        return (self.width + self.height) * 2
-    
-    def print_info(self):
-        info_list1 = [self.width, self.height, self.calculate_area(), self.calculate_perimeter()]
-        info_list2 = ['Width', 'Height', 'Area', 'Perimeter']
-        i = 0
-        j = 0
-        while i < len(info_list1):
-            print(f'{info_list1[j]} : {info_list2[j]}')
-            i += 1
-            j += 1
+class Dog:
+   pass
 
-shape1 = Shape(5, 3)
-
-shape1.print_info()
+dog1 = Dog()
+dog1.bark()
