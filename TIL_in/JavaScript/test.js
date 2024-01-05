d2e29f91b304e58233cf94e872af7cd79bfedf5a
tmdwoghd <tmdwoghd@naver.com>
2024-01-05 17:59:06 +0900

    0105

--- TIL_in/JavaScript/test.js
+++ TIL_in/JavaScript/test.js
@@ -1,1 +1,18 @@
-console.log('hello world');
+// for..in vs for..of
+
+const arr = []
+arr[4] = 4
+arr[1] = 1
+arr[2] = 2
+
+// 배열을 순회할때는 in 사용 xxxx
+for (const i in arr) {
+  console.log(i)
+}
+
+console.log("============")
+
+for (const i of arr) {
+  console.log(i)
+  
+}
